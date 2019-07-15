76d05052bc4a1b9202a1940dfbae090bdd58a107
chenyoulin1993 <chenyoulin1993@hotmail.com>
2019-07-15 20:56:48 +0800

    update 4

--- src/components/City/City.vue
+++ src/components/City/City.vue
@@ -1,25 +1,35 @@
 <template>
     <div class="city">
         <cityHeader></cityHeader>
-        <cityHot></cityHot>
-        <cityAlphabet></cityAlphabet>
-        <cityList></cityList>
+        <cityList :cityList = 'cityList' :cityHot = 'cityHot'></cityList>
     </div>
 </template>
 
 <script>
 
 import cityHeader from './Pages/Header'
-import cityHot from './Pages/Hotcity'
-import cityAlphabet from './Pages/Alphabet'
 import cityList from './Pages/Citylist'
 
 export default {
     components:{
         cityHeader,
-        cityHot,
-        cityAlphabet,
         cityList
+    },
+    data(){
+        return{
+            cityHot:[],
+            cityList:{}
+        }
+    },
+    mounted(){
+
+        this.$http.get('/api/city.json')
+        .then((res)=>{
+            let data = res.data.data;
+            this.cityHot = data.hotCities;
+            this.cityList = data.cities;
+        })
+
     }
 }
 </script>
--- src/components/City/Pages/Citylist.vue
+++ src/components/City/Pages/Citylist.vue
@@ -1,31 +1,89 @@
 <template>
-    <div class="list">
+    
+            <div ref="wrapper" class="wrapper">
+                <div class="content">
+             <div class="hot">
+                <div class="hot-title">热门城市</div>
+                <ul class="hot-list">
+                   <li class="hot-item" v-for="item in cityHot" :key="item.id">
+                       {{ item.name }}
+                   </li>
+                </ul>
+            </div>
+            
 
-        <div class="list-title">A</div>
-        <ul class="list-area">
-            <li class="list-item">阿坝藏族羌族自治州</li>
-            <li class="list-item">阿克苏地区</li>
-            <li class="list-item">阿拉尔</li>
-            <li class="list-item">阿拉善盟</li>
-            <li class="list-item">阿克苏</li>
-            <li class="list-item">阿克苏</li>
-            <li class="list-item">阿克苏</li>
-            <li class="list-item">阿克苏</li>
-            <li class="list-item">阿克苏</li>
-            <li class="list-item">阿克苏</li>
-           
+
+            <div class="alphabet">
+        <div class="alphabet-title">字母排序</div>
+        <ul class="alphabet-list">
+          
+          
+          <li class="alphabet-item" v-for="(val,key) in cityList" v-on:click="changeSort(key)">
+              {{ key }}
+              </li>
         </ul>
 
+    </div>
+
+
+
+
+            <div class="list">
+                <div v-for="(val,key) in cityList" :ref="key" >
+                    <div class="list-title">{{key}}</div>
+                    <ul class="list-area">
+                    <li class='list-item' v-for="item in val" :key="item.id">
+                        {{ item.name }}
+                    </li>        
+                    </ul>
+                </div>
+            </div>
+
+
+
+        </div>
     </div>
 </template>
 
 <script>
+import BScroll from '@better-scroll/core'
 export default {
+    props:['cityList','cityHot'],
+    
+    data(){
+        
+        return{
+            scroll:' ',
+        }
+    },
+    mounted(){
+ 
+        let wrapper = this.$refs['wrapper']
+        this.scroll = new BScroll(wrapper)
 
+    },
+    methods:{
+        changeSort(sortName){
+
+            console.log(this.$refs)
+
+        }
+    }
+
+    
+    
 }
 </script>
 
 <style scoped>
+
+.wrapper{
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom:0;
+    top:.88rem;
+}
 
 
 .list-title{
@@ -75,7 +133,55 @@ export default {
     border-left: .02rem solid #ddd;
     border-right: .02rem solid #ddd;
 }
+.hot-title{
+    font-size: .24rem;
+    color: #212121;
+    padding: .2rem .3rem;
+}
+.hot-list{
+    position: relative;
+    overflow: hidden;
+    background: #fff;
+    font-size: .28rem;
+    color: #212121;
+}
+.hot-list::before{
+    content: " ";
+    position: absolute;
+    height: 100%;
+    width: 33.33%;
+    left:33.33%;
+    border-left:.02rem solid #ddd ;
+    border-right:.02rem solid #ddd ;
+}
+.hot-item{
+    width: 33.33%;
+    text-align: center;
+    height: .9rem;
+    line-height: .9rem;
+    float: left;
+    border-bottom:.02rem solid #ddd;
+}
+.alphabet-title{
+    font-size: .24rem;
+    color: #212121;
+    padding: .2rem .3rem;
+}
+.alphabet-list{
+    position: relative;
+    overflow: hidden;
+    background: #fff;
+    font-size: .28rem;
+    color: #212121;
+}
 
+.alphabet-item{
+    width: 16.666%;
+    text-align: center;
+    height: .9rem;
+    line-height: .9rem;
+    float: left;
+}
 
 
 </style>
